8420f561f250b0faf888f3109d34e956e2a47ed4
Yashraj595 <ys2391356@gmail.com>
2026-01-13 13:09:37 +0530

    fixing form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,33 +6,38 @@ const Contact = () => {
   return (
     <>
       <Navigation />
-
+      
       <div id="contactouter">
+        <h2>Connection Form</h2>
         <div id="contatinner">
-         
-          
+          <form>
+            <input
+              className="forminput"
+              type="text"
+              placeholder="Enter your name"
+            />
 
-      <form>
-        
-        
-        <input type="text" placeholder="Enter your name" />
+            <input
+              className="forminput"
+              type="email"
+              placeholder="Enter your email"
+            />
 
-        
-        
-        <input type="email" placeholder="Enter your email" />
+            {/* <label>Phone</label><br /> */}
+            <input
+              className="forminput"
+              type="tel"
+              placeholder="Enter your phone number"
+            />
 
-        
-        {/* <label>Phone</label><br /> */}
-        <input type="tel" placeholder="Enter your phone number" />
+            {/* <label>Message</label><br /> */}
+            <textarea
+              className="forminput"
+              placeholder="Write your message"
+            ></textarea>
 
-        
-        {/* <label>Message</label><br /> */}
-        <textarea placeholder="Write your message"></textarea>
-
-        
-        <button type="submit">Submit</button>
-      </form>
-          
+            <button id='submitbutton' type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </>
